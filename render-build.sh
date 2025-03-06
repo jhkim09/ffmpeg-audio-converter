8bc4c45db79931f79283586fb71a0d472281080c
jhkim09 <newsh11@gmail.com>
2025-03-06 09:19:36 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
+mkdir -p ~/ffmpeg && cd ~/ffmpeg
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xvf ffmpeg-release-amd64-static.tar.xz
+mv ffmpeg-*-static ffmpeg_bin
+export PATH=$PATH:~/ffmpeg/ffmpeg_bin
