bb2721ba9e48558037c4ecf9aaca338f20cf7fe3
jhkim09 <newsh11@gmail.com>
2025-03-06 09:33:26 +0900

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# FFmpeg 바이너리 다운로드 경로 변경
+# FFmpeg 다운로드 및 설치 (올바른 다운로드 경로 사용)
 mkdir -p /opt/render/ffmpeg && cd /opt/render/ffmpeg
-wget -O ffmpeg.tar.xz "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.tar.xz"
+wget -O ffmpeg.tar.xz "https://www.johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
 tar -xvf ffmpeg.tar.xz --strip-components=1
 export PATH=$PATH:/opt/render/ffmpeg
