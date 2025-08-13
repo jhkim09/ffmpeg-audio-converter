ad9e015690f225698deac500e47f3a0ace295a65
jhkim09 <newsh11@gmail.com>
2025-08-13 17:13:29 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-# 🔹 FFmpeg 설치 디렉토리
 FFMPEG_DIR="/opt/render/ffmpeg"
-
-# 🔹 설치 및 환경변수 설정
+rm -rf $FFMPEG_DIR
 mkdir -p $FFMPEG_DIR && cd $FFMPEG_DIR
-curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-tar -xf ffmpeg.tar.xz --strip-components=1
-export PATH=$FFMPEG_DIR:$PATH
 
-# 🔹 FFmpeg 정상 설치 여부 확인
+wget -O ffmpeg.tar.xz "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
+tar -xvf ffmpeg.tar.xz --strip-components=1
+rm -f ffmpeg.tar.xz
+
+export PATH=$FFMPEG_DIR:$PATH
 ffmpeg -version
+
+# Flask + Celery 실행
+gunicorn -w 4 -b 0.0.0.0:5000 app:app & celery -A celery_worker worker --loglevel=info --pool=solo --concurrency=2
