a8192b3d3a086308d1103d48f9cde2aff38d4eb6
jhkim09 <newsh11@gmail.com>
2025-08-13 17:41:45 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e
 
+# FFmpeg 설치
 FFMPEG_DIR="/opt/render/ffmpeg"
-rm -rf $FFMPEG_DIR
-mkdir -p $FFMPEG_DIR && cd $FFMPEG_DIR
-
+rm -rf "$FFMPEG_DIR"
+mkdir -p "$FFMPEG_DIR" && cd "$FFMPEG_DIR"
 wget -O ffmpeg.tar.xz "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
 tar -xvf ffmpeg.tar.xz --strip-components=1
 rm -f ffmpeg.tar.xz
-
-export PATH=$FFMPEG_DIR:$PATH
+export PATH="$FFMPEG_DIR:$PATH"
 ffmpeg -version
 
-# Flask + Celery 실행
-
+# 서버 & 워커 실행
+# - Upstash 연결과 충돌 줄이기 위해 Celery를 solo/pool 제한 + 낮은 concurrency
 gunicorn -w 4 -b 0.0.0.0:5000 app:app &
 celery -A celery_worker worker --loglevel=info --pool=solo --concurrency=2
-
