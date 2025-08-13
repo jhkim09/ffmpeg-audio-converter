f79a0de688b524a0676606b8f255f04651e1ebf0
jhkim09 <newsh11@gmail.com>
2025-08-13 17:55:47 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -16,9 +16,3 @@ rm -f ffmpeg.tar.xz
 
 # back to project dir
 popd
-
-
-# 서버 & 워커 실행
-# - Upstash 연결과 충돌 줄이기 위해 Celery를 solo/pool 제한 + 낮은 concurrency
-gunicorn -w 4 -b 0.0.0.0:5000 app:app &
-celery -A celery_worker worker --loglevel=info --pool=solo --concurrency=2
