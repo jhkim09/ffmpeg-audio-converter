231531e8d0ae8adf22c824e73db59ee7ffa50861
jhkim09 <newsh11@gmail.com>
2025-08-13 17:52:11 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
-# FFmpeg 설치
+# remember current dir (Render 기본 작업경로: /opt/render/project/src)
+pushd .
+
 FFMPEG_DIR="/opt/render/ffmpeg"
 rm -rf "$FFMPEG_DIR"
-mkdir -p "$FFMPEG_DIR" && cd "$FFMPEG_DIR"
+mkdir -p "$FFMPEG_DIR"
+cd "$FFMPEG_DIR"
+
+# Install FFmpeg (static build)
 wget -O ffmpeg.tar.xz "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
 tar -xvf ffmpeg.tar.xz --strip-components=1
 rm -f ffmpeg.tar.xz
-export PATH="$FFMPEG_DIR:$PATH"
-ffmpeg -version
+
+# back to project dir
+popd
+
 
 # 서버 & 워커 실행
 # - Upstash 연결과 충돌 줄이기 위해 Celery를 solo/pool 제한 + 낮은 concurrency
